c2aedd180f86734489fffab4b45914bccfdc043f
dxcloud <chengwhuang@gmail.com>
2013-04-09 10:50:56 +0100

    Method run() implemented

--- SaC2/sac2_engine.cpp
+++ SaC2/sac2_engine.cpp
@@ -1,4 +1,5 @@
 ﻿#include "sac2_engine.hpp"
+#include "sac2_menu_state.hpp"
 
 namespace sac2
 {
@@ -28,10 +29,17 @@ sac2_status_t Engine::init()
 
   m_window.SetFramerateLimit(DEFAULT_FRAMERATE_LIMIT);
 
+  GameState* state(new MenuState(this));
+  m_state_manager.add_state(state);
+
+#ifdef LOG_ENABLED
+  m_log << "Engine::init() >> completed" << std::endl;
+#endif
+
   return STATUS_SUCCESS;
 }
 
-void Engine::process_arguments(int argc, char* argv[])
+void Engine::parse_options(int argc, char* argv[])
 {
 #ifdef LOG_ENABLED
   m_log << "Engine::process arguments:" << std::endl;
@@ -42,14 +50,63 @@ void Engine::process_arguments(int argc, char* argv[])
 
 sac2_status_t Engine::run()
 {
-  m_running = true;
+#ifdef LOG_ENABLED
+  m_log << "Engine::run() started" << std::endl;
+#endif
+
+  m_running = true;  // The Engine is running
 
   sac2_status_t status(STATUS_SUCCESS);
   status = init();
 
-  if (STATUS_SUCCESS != status)
+  if (STATUS_SUCCESS != status) {
+#ifdef LOG_ENABLED
+  m_log << "Engine::init() initialization failed" << std::endl;
+#endif
     return status;
+  }
 
+  status = loop();  // Main loop
+
+  if (STATUS_SUCCESS != status) {
+#ifdef LOG_ENABLED
+  m_log << "Engine::loop() quit with " << status << std::endl;
+#endif
+    return status;
+  }
+
+  status = cleanup();
+
+#ifdef LOG_ENABLED
+  m_log << "Engine::run() ended with " << status << std::endl;
+#endif
+
+  return status;
+}
+
+sac2_status_t Engine::loop()
+{
+  while (true == m_running) {
+    GameState* state = m_state_manager.get_state();
+    if (0 == state) return STATUS_ERROR;
+
+    sf::Event event;
+
+    if (true == m_window.GetEvent(event)) {
+      if (sf::Event::Closed == event.Type) {
+	quit();
+      }
+      else {
+	state->handle_events(event);
+      }
+    }  // if an event happened
+    if (false == state->is_paused()) {
+      state->update();
+      state->draw();
+      state = 0;
+      m_window.Display();
+    }  // if the current state is active
+  }  // Loop until m_running is true
   return STATUS_SUCCESS;
 }
 
@@ -78,15 +135,5 @@ sac2_status_t Engine::cleanup()
 #endif
   return STATUS_SUCCESS;
 }
-
-  sac2_status_t Engine::update()
-  {
-    return STATUS_SUCCESS;
-  }
-
-  sac2_status_t Engine::draw()
-  {
-    return STATUS_SUCCESS;
-  }
 
 }
