8464f7b751dae1ae862c905ab3f547a41f72070f
dxcloud <chengwhuang@gmail.com>
2013-04-10 16:54:57 +0100

    Script for setting up SaC2 environment variables

---
+++ SaC2/sac2.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+### Set environment variables
+
+### Path to this file
+SAC_PATH="$PWD"
+### Path of the generic Makefile and Makerules directory
+SAC_MAKE=$SAC_PATH/make
+
+export SAC_PATH
+export SAC_MAKE
+
+echo "SaC2 set up"
