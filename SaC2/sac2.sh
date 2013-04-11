b585983d7c428da81c0231becd5f79a8c9033674
dxcloud <chengwhuang@gmail.com>
2013-04-11 13:36:55 +0100

    Script for setting variables

--- SaC2/sac2.sh
+++ SaC2/sac2.sh
@@ -1,12 +1,19 @@
-#! /bin/bash
-### Set environment variables
+#!/bin/bash
+### Set SaC2 environment variables
 
 ### Path to this file
-SAC_PATH="$PWD"
+SAC_MAKE_DIR=$(locate -l 1 SaC2/make)
+SAC_DIR=$(dirname $SAC_MAKE_DIR)
+
 ### Path of the generic Makefile and Makerules directory
-SAC_MAKE=$SAC_PATH/make
+SAC_MAKE=$SAC_MAKE_DIR/rules.mk
 
-export SAC_PATH
-export SAC_MAKE
-
-echo "SaC2 set up"
+### Test if makefile.mk exist
+if [ ! -e $SAC_MAKE ]; then
+  echo "$SAC_MAKE: Not such file"
+else
+  export SAC_DIR
+  export SAC_MAKE_DIR
+  export SAC_MAKE
+  echo "SaC2 set up"
+fi
