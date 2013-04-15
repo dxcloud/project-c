798ab6a44e744b009d5e6989dfd587020fb840f3
dxcloud <chengwhuang@gmail.com>
2013-04-15 04:58:41 +0100

    main.cpp and Makefile updated

--- SaC2/apps/app02/main.cpp
+++ SaC2/apps/app02/main.cpp
@@ -5,6 +5,7 @@ using namespace sac2;
 int main(int argc, char** argv)
 {
   Engine app("First game state");
+  app.parse_options(argc, argv);
 
   StateManager* p_state_manager = StateManager::get_instance();
   p_state_manager->add_state(new MenuState);
