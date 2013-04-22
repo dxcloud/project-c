7d8598a724e5a459549e4730dd03388cf4bd9df0
dxcloud <chengwhuang@gmail.com>
2013-04-22 12:45:15 +0100

    Image samples added

--- SaC2/src/sac2_resources.cpp
+++ SaC2/src/sac2_resources.cpp
@@ -6,6 +6,10 @@ sac2_asset_map_t create_image()
 {
   sac2_asset_map_t map_image;
   map_image.insert(std::make_pair("IM01", "/home/orange/project-c/SaC2/samples/image01.png"));
+  map_image.insert(std::make_pair("IM02", "/home/orange/project-c/SaC2/samples/image02.png"));
+  map_image.insert(std::make_pair("IM03", "/home/orange/project-c/SaC2/samples/image03.png"));
+  map_image.insert(std::make_pair("IM04", "/home/orange/project-c/SaC2/samples/image04.png"));
+  map_image.insert(std::make_pair("IM05", "/home/orange/project-c/SaC2/samples/image05.png"));
   return map_image;
 }
 
