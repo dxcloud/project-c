219c20ae974aa3c579aad61ed288fda9cd9fbd8b
dxcloud <chengwhuang@gmail.com>
2013-06-18 15:19:14 +0100

    Environment variables renamed

--- SaC2/sac2.sh
+++ SaC2/sac2.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 ### Set SaC2 environment variables
+### Author:      Chengwu Huang
+### Last update: 2013-06-18
 
-### Path to this file
-SAC_MAKE_DIR=$(locate -l 1 SaC2/make)
-SAC_DIR=$(dirname $SAC_MAKE_DIR)
-
-### Path of the generic Makefile and Makerules directory
-SAC_MAKE=$SAC_MAKE_DIR/rules.mk
-
-### Test if makefile.mk exist
-if [ ! -e $SAC_MAKE ]; then
-  echo "$SAC_MAKE: Not such file"
-else
-  export SAC_DIR
-  export SAC_MAKE_DIR
-  export SAC_MAKE
-  echo "SaC2 set up"
+SAC2_MAKERULES=$(locate -l 1 SaC2/make/rules.mk)
+if [ ! -e $SAC2_MAKERULES ]; then
+  echo "Error: unable to locate file \`SaC2/make/rules.mk'"
+  return
 fi
+
+SAC2_MAKE_PATH=$(dirname $SAC2_MAKERULES)
+SAC2_PATH=$(dirname $SAC2_MAKE_DIR)
+SFML_PATH=$SAC2_PATH/libs/SFML-2.0
+
+echo "Exporting \`SAC2_PATH=$SAC2_PATH'..."
+export SAC2_PATH
+echo "Exporting \`SAC2_MAKE_PATH=$SAC2_MAKE_PATH'..."
+export SAC2_MAKE_PATH
+echo "Exporting \`SAC2_MAKERULES=$SAC2_MAKERULES'..."
+export SAC2_MAKERULES
+echo "Exporting \`SFML_PATH=$SFML_PATH'..."
+export SFML_PATH
+
+echo "SaC2 environment variables set up"
+
