a9361e4928c13b3bbddb673741bf068881a10917
dxcloud <chengwhuang@gmail.com>
2013-07-28 13:40:45 +0100

    define common color

---
+++ SaC2/include/types/sac2_color.hpp
@@ -0,0 +1,29 @@
+#ifndef SAC2_COLOR_HPP
+#define SAC2_COLOR_HPP
+
+#include <SFML/Graphics/Color.hpp>
+
+namespace sac2
+{
+
+typedef sf::Color color_t;
+
+const color_t COLOR_RED         = sf::Color::Red;
+const color_t COLOR_GREEN       = sf::Color::Green;
+const color_t COLOR_BLUE        = sf::Color::Blue;
+const color_t COLOR_YELLOW      = sf::Color::Yellow;
+const color_t COLOR_WHITE       = sf::Color::White;
+const color_t COLOR_BLACK       = sf::Color::Black;
+const color_t COLOR_TRANSPARENT = sf::Color::Transparent;
+const color_t COLOR_GRAY        = sf::Color(128, 128, 128);
+const color_t COLOR_SILVER      = sf::Color(192, 192, 192);
+const color_t COLOR_CYAN        = sf::Color::Cyan;
+const color_t COLOR_GOLD        = sf::Color(255, 215, 0);
+const color_t COLOR_INDIGO      = sf::Color(75, 0, 130);
+const color_t COLOR_ORANGE      = sf::Color(255, 127, 0);
+const color_t COLOR_MAGENTA     = sf::Color::Magenta;
+const color_t COLOR_MAROON      = sf::Color(128, 0, 0);
+
+}
+
+#endif  //! SAC2_COLOR_HPP
