a076780f8a165c8184ab1e073effc844e2471748
dxcloud <chengwhuang@gmail.com>
2014-05-03 14:48:29 +0200

    moved to root include directory

---
+++ SaC2/include/sac2.hpp
@@ -0,0 +1,54 @@
+//////////////////////////////////////////////////////////////////////////////
+//! \file    sac2.hpp
+//! \author  Chengwu HUANG
+//! \version 0.1
+//! \date    2013-04-05 - Initial Development
+//! \brief   Provides all includes
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef SAC2_HPP
+#define SAC2_HPP
+
+//////////////////////////////////////////////////////////////////////////////
+//! \mainpage
+//!     Simple and Clear 2D Engine API
+//! \author
+//!     Chengwu HUANG
+//! \version
+//!     0.1 (alpha)
+//! \date
+//!     2013-04-05: initial development date
+//! \section Introduction
+//!     SaC2 Engine tends to simply manipulation.
+//////////////////////////////////////////////////////////////////////////////
+
+// SaC2 Types include
+#include <sac2_type.hpp>
+#include <sac2_color.hpp>
+#include <sac2_key.hpp>
+
+// SaC2 Core include
+#include <sac2_engine.hpp>
+#include <sac2_logger.hpp>
+#include <sac2_input_config.hpp>
+#include <sac2_input_manager.hpp>
+#include <sac2_state_manager.hpp>
+
+// SaC2 Interface include
+#include <sac2_asset.hpp>
+#include <sac2_manager.hpp>
+#include <sac2_game_state.hpp>
+
+// SaC2 Assets include
+#include <sac2_asset_manager.hpp>
+#include <sac2_audio_manager.hpp>
+#include <sac2_graphic_asset.hpp>
+#include <sac2_scene.hpp>
+#include <sac2_sprite_asset.hpp>
+
+// SaC2 Utilities include
+#include <sac2_math.hpp>
+#include <sac2_physics.hpp>
+
+#endif  //! SAC2_HPP
+
