da1c03c0ec890d6af14344bddc283671a8756b9f
dxcloud <chengwhuang@gmail.com>
2014-07-04 17:31:49 +0200

    add SDL return value check macros

--- Icelos/include/icelos_macros.hpp
+++ Icelos/include/icelos_macros.hpp
@@ -11,10 +11,42 @@ namespace icelos
 
 #if defined(ICELOS_DEBUG)
 #  define OUT(X) std::cout << X << std::endl;
+#  define ICELOS_SDL_CHECK(X) \
+          std::cout << "SDL CHECK" << std::endl; \
+          if (0 > X) { \
+            std::cout << SDL_GetError() << std::endl; \
+            return STATUS_FAIL; \
+          }
+
+#  define ICELOS_SDL_FLAGS_CHECK(X, flags) \
+          if (flags != (X & flags)) { \
+            std::cout << SDL_GetError() << std::endl; \
+            return STATUS_FAIL; \
+          }
+
+#  define ICELOS_SDL_PTR_CHECK(X) \
+          if (nullptr == X) { \
+            std::cout << SDL_GetError() << std::endl; \
+            return STATUS_FAIL; \
+          }
+
 #else
 #  define OUT(X)
+#  define ICELOS_SDL_CHECK(X) X;
+#  define ICELOS_SDL_FLAGS_CHECK(X, flags) X;
+#  define ICELOS_SDL_PTR_CHECK(X)
 #endif
 
+#define ICELOS_WINDOW_WIDTH  RenderManager::instance().get_size().w
+#define ICELOS_WINDOW_HEIGHT RenderManager::instance().get_size().h
+
+#define ICELOS_BIT(X) 1 << X
+
+#define ICELOS_INIT_CHECK(X) \
+        if (false == X) { return STATUS_FAIL; }
+
 }
+
+// using icelos;
 
 #endif // ICELOS_MACROS_HPP
